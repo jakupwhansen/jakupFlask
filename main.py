2db3012fd6a5900c5258c77aa2bad27016d004ab
Jákup Wenningstedt Hansen <jakupwhansen@hotmail.com>
2019-01-31 21:55:19 +0100

    bla

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Importeret Keras.models fjernet'
+  return 'My name is Jakup'
 
 if __name__ == '__main__':
   app.run()
