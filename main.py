2e55b3246d2c442b6137bfa0985fa4c430040226
Jákup Wenningstedt Hansen <jakupwhansen@hotmail.com>
2019-01-31 21:34:37 +0100

    keras text

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from keras.models import Sequential
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'My name is Jakup'
+  return 'Importeret Keras.models'
 
 if __name__ == '__main__':
   app.run()
